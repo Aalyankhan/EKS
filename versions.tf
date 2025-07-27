f9c4e98daab1ef37e9dc091fee6446c2b929ec70
Aalyankhan <aalyankhan029@gmail.com>
2025-07-27 19:58:38 +0500

    Adding fixing  git

---
+++ versions.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.35"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.82"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3"
+    }
+  }
+}
